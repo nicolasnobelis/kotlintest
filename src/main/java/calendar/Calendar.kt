87a6225c74c940b88f1e484516da5a632cdbd112
Nicolas Nobelis <Nicolas.Nobelis@wolterskluwer.com>
2019-01-04 16:17:38 +0100

    more work

--- src/main/java/calendar/Calendar.kt
+++ src/main/java/calendar/Calendar.kt
@@ -1,14 +1,30 @@
 package calendar
 
+import java.time.LocalDateTime
+
 class Calendar(val name:String, val owner:Owner ) {
     var active:Boolean = true
+    // in case there are two appointments with the same date, we use the title as disambiguation
+    private val appointments: MutableSet<Appointment> = sortedSetOf(compareBy ({ it.date }, {it.title}))
 
     constructor(name:String, owner: Owner, active:Boolean ) : this(name, owner) {
         this.active = active
     }
+
+    // read only set over appointments
+    fun getAppointments() : Set<Appointment> {
+        return appointments.toSet()
+    }
+
+    fun addAppointments(element:Appointment) {
+        val result = appointments.add(element)
+        println("added ${element.title} : ${result}")
+    }
 }
 
-data class Owner(val firstname:String, val lastname:String);
+data class Owner(val firstname:String, val lastname:String)
+
+data class Appointment (val title:String,  val description:String, val date:LocalDateTime)
 
 
 
--- src/test/java/Tests.kt
+++ src/test/java/Tests.kt
@@ -1,7 +1,10 @@
+import calendar.Appointment
 import calendar.Calendar
 import calendar.Owner
 import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
 class Tests {
@@ -9,21 +12,63 @@ class Tests {
         return Owner("Max", "Mustermann")
     }
 
-    @Test
-    fun testOwner() {
-        val owner = createOwner()
+    fun createAppointment(title: String = "RDV Dentist"): Appointment {
+        return Appointment(title, "Appointment at the dentist", LocalDateTime.now())
+    }
+
+    fun createCalendar(): Calendar {
+        return Calendar("test Calendar", createOwner())
+    }
+
+    fun assertOwner(owner:Owner) {
+        assertNotNull(owner)
         assertEquals("Max", owner.firstname)
         assertEquals("Mustermann", owner.lastname)
     }
 
     @Test
-    fun testCalendar() {
+    fun testOwner() {
         val owner = createOwner()
-        val calendar = Calendar("test Calendar", createOwner())
+        assertOwner(owner)
+    }
+
+    @Test
+    fun testCalendar() {
+        val calendar = createCalendar()
 
         assertEquals("test Calendar", calendar.name)
         assertTrue { calendar.active }
-        assertEquals(owner, calendar.owner)
+        assertOwner(calendar.owner)
     }
 
+    @Test
+    fun testAppointment() {
+        val appointment = createAppointment()
+        val calendar = createCalendar()
+
+        assertTrue(calendar.getAppointments().isEmpty())
+
+        calendar.addAppointments(appointment)
+
+        val appointments = calendar.getAppointments()
+        assertEquals(1, appointments.size)
+    }
+
+    @Test
+    fun testAppointmentOrdering() {
+        val appointment1 = createAppointment("Test 1")
+        val appointment2 = createAppointment("Test 2")
+        val calendar = createCalendar()
+
+        assertTrue(calendar.getAppointments().isEmpty())
+
+        calendar.addAppointments(appointment2)
+        calendar.addAppointments(appointment1)
+
+        val appointments = calendar.getAppointments()
+        val appointmentsList = appointments.toList();
+        assertEquals(2, appointments.size)
+        assertEquals("Test 1", appointmentsList.get(0).title)
+        assertEquals("Test 2", appointmentsList.get(1).title)
+    }
 }
